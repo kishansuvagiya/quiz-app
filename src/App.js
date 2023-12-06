2f649cd48e6e16adfccbeff35b7197eb91d63c03
kishansuvagiya <suvagiya.kishan96@gmail.com>
2023-12-06 11:01:19 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { ToastContainer } from 'react-toastify';
+import Login from './pages/Login';
+import Category from './pages/Category';
+import Quiz from './pages/Quiz';
+import Protect from './Protect';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Login />} />
+          <Route path='/category' element={<Protect><Category /></Protect>} />
+          <Route path='/quiz/:name' element={<Protect><Quiz /></Protect>} />
+        </Routes>
+        <ToastContainer />
+      </BrowserRouter>
+
     </div>
   );
 }
